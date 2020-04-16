8d4aefe2e298e628423052323a02067e9e76b411
Dylan <dhite@cs.uoregon.edu>
2020-04-15 19:11:14 -0700

    First Commit

---
+++ Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    public Transform enemy;
+    public CanvasGroup enemyDetectionImages;
+
+    bool m_EnemyInRange = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == enemy)
+        {
+            m_EnemyInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == enemy)
+        {
+            m_EnemyInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (m_EnemyInRange)
+        {
+            //Calculate dot product to find angle between enemy and Johnlemon
+            //Display appropriate image for the angle the enemy is in relation to John
+            //Use linear interp to find distance between enemy and John
+            //Change intensity of light based on distance to enemy
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+### Assignment 2 CIS 410
+
+I did not include uses of dot product, linear interpolation or particle effects.
+
+I did have an idea to detect enemies nearby John Lemon and use images to display direction
+and distance warnings to the players screen. I unfortunately ran out of time and did not finish
+implementing these.
+
+I did start to implement these features as seen in the John Lemon prefab there is an enemy field
+with a capsule colider to detect enemies. The starting script called EnemyDetection has the logic
+which is very similar to the GameEnding script as in it detects an enemy character, then updates the images
+accordingly. 
+
+Team Members:
+Dylan Hite
